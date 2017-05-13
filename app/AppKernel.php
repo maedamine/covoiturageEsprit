2e9b57c369fdce861dc1cd1b1ce15816aed124ec
CHAMS <chamseddine.bezzaouia@gmail.com>
2017-05-13 12:19:11 +0100

    FosUser Bundle non personnalisé

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new CovoiturageBundle\CovoiturageBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MyApp\UserBundle \Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+
+ * @ORM\Entity
+
+ * @ORM\Table(name="utilisateur")
+
+ */
+
+class User extends BaseUser
+
+{
+
+    /**
+
+     * @ORM\Id
+
+     * @ORM\Column(type="integer")
+
+     * @ORM\GeneratedValue(strategy="AUTO")
+
+     */
+
+    protected $id;
+
+}
